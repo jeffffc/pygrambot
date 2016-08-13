35736beafe191706af9f585feafa74ae897434f5
Cauê Baasch de Souza <caue@antergos.home>
2016-08-13 15:56:38 -0300

    Added logging

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
-import os
+import os, logging
 from telegram.ext import Updater, CommandHandler
+
+logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 TOKEN = os.environ.get('TOKEN')
 APPNAME = os.environ.get('APPNAME')
-
 PORT = int(os.environ.get('PORT', '5000'))
 updater = Updater(TOKEN)
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
+updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
 updater.bot.setWebhook("https://"+APPNAME+".herokuapp.com/"+TOKEN)
 
 
