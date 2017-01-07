0b0acf33aa90a0e484cf8053f0bd77753d01beb5
Caue Baasch de Souza <cauebaasch@gmail.com>
2017-01-06 23:47:36 -0200

    Just minor changes, mostly formatting

--- bot.py
+++ bot.py
@@ -1,22 +1,26 @@
-import os, logging
+import os
+import logging
 from telegram.ext import Updater, CommandHandler
 
-logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(name)s - %(message)s')
 
-TOKEN = os.environ.get('TOKEN')
-APPNAME = os.environ.get('APPNAME')
-PORT = int(os.environ.get('PORT', '5000'))
-updater = Updater(TOKEN)
-updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-updater.bot.setWebhook("https://"+APPNAME+".herokuapp.com/"+TOKEN)
+token = os.environ.get('TOKEN')
+appname = os.environ.get('APPNAME')
+port = int(os.environ.get('PORT', '5000'))
+
+updater = Updater(token)
+updater.start_webhook(listen="0.0.0.0", port=port, url_path=token)
+updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(appname, token))
 
 
 def start(bot, update):
     bot.sendMessage(update.message.chat_id, text='Hello World!')
 
+
 def hello(bot, update):
-    bot.sendMessage(update.message.chat_id,
-                    text='Hello '+update.message.from_user.first_name)
+    first_name = update.message.from_user.first_name
+    update.message.reply_text('Hello {}'.format(first_name))
+
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
